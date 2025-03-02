a8ef3cb9fb15c982a9a984bf80b1921bf3e90f6f
Vishakh Warrier <vvishakhp@gmail.com>
2025-03-02 15:58:48 +0530

    Update required packages list

--- setup.sh
+++ setup.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-packagesNeeded=(ansible git)
-
 if [ -x "$(command -v apk)" ];
 then
-    sudo apk add --no-cache "${packagesNeeded[@]}"
+    sudo apk add --no-cache ansible git
 elif [ -x "$(command -v apt-get)" ];
 then
-    sudo apt-get install "${packagesNeeded[@]}"
+    sudo apt-get install ansible git
 elif [ -x "$(command -v dnf)" ];
 then
-    sudo dnf install "${packagesNeeded[@]}"
+    sudo dnf install ansible git
 elif [ -x "$(command -v zypper)" ];
 then
-    sudo zypper install "${packagesNeeded[@]}"
+    sudo zypper install ansible git
 else
     echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: "${packagesNeeded[@]}"">&2;
 fi
 
-sudo ansible-pull https://github.com/vvishakhp/dev-env.git
+sudo ansible-pull --url https://github.com/vvishakhp/dev-env.git
 
